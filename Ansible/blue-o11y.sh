3e4fe32ff367153fb7857488ae01b0124a9b83e7
mvolejnik <mvolejnik@github.com>
2025-12-27 20:42:10 +0100

    174 Caddy proxy

---
+++ Ansible/blue-o11y.sh
@@ -0,0 +1,1 @@
+ansible-playbook all.yaml --limit o11y-blue -i inventory.yaml --extra-vars @.vault.yaml
---
+++ Ansible/setup-o11y.sh
@@ -0,0 +1,1 @@
+ansible-playbook setup-o11y.yaml --limit localhost  -i inventory.yaml --extra-vars @.vault.yaml -v
